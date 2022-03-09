6659c7c9c51ee17fa0411ddc08c75301711b9ab6
محمد الصوالحي <ms.2052001@gmail.com>
2022-03-09 12:17:23 +0200

    refactor the menu builder bot automation script

--- automate-menu-builder-bot.py
+++ automate-menu-builder-bot.py
@@ -14,10 +14,14 @@ logging.basicConfig(format='[%(levelname)-7s %(asctime)s] %(name)s: %(message)s'
 BOT_USERNAME = 'Da7ee7_Civil_1st_Year_Bot'
 env = dotenv_values(".env")
 telegram_client = TelegramClient('telethon', env.get(
-    "TELEGRAM_API_ID"), env.get("TELEGRAM_API_HASH"))
-process = deque([
-    # "/start",
-    # "/langen",
+    "TELEGRAM_API_ID"), env.get("TELEGRAM_API_HASH"))  # type: ignore
+
+# we need this global `error_occured` because we are running different threads
+# and async code if an error occured, we should stop the next process step
+error_occured = False
+process: deque[str | dict[str, str]] = deque([
+    "/start",
+    "/langen",
 ])
 
 with open("./menu_buttons/main.json", "r") as file:
@@ -72,46 +76,75 @@ process.append("/langar")
 # print(*process, sep="\n")
 # exit()
 
+process = deque([
+    "🎛 Buttons Editor",
+    "➕ Add Button",
+    "الترم الأول a",
+    "➕ Add Button",
+    "الترم الثاني a",
+    {'type': 'click-button', 'name': '⬆️'}
+])
+
 
 def get_message():
+    """ get the next process step, return None if an error occured occured
+    we ran out of the step and all are done """
+
     try:
         message = process.popleft()
+        if len(process) == 0:
+            logging.info("Alhamdulilah, all are done 💚")
+        return message
     except:
-        logging.info("Alhamdulilah, all are done 💚")
         return
-    return message
 
 
-@debounce_async(0.1)
-async def send_message(event=None):
+async def click_inline_button(button_name: str, event: events.NewMessage.Event | None):
+    global error_occured
+    if not event:
+        logging.error("an event is required to click an inline button")
+        error_occured = True
+        return
+
+    buttons = await event.get_buttons()
+
+    if not buttons:
+        logging.error("can't find buttons to click: " +
+                      button_name)
+        error_occured = True
+        return
+    for buttons_row in buttons:
+        for button in buttons_row:
+            if button_name == button.button.text:
+                logging.info("clicking: " + button.button.text)
+                await button.click()
+
+
+@debounce_async(0.3)
+async def send_message(event: events.NewMessage.Event | None = None):
     message = get_message()
-    if not message:
-        await telegram_client.disconnect()  # we are done
-        return
-    if type(message) == str:
+    if type(message) is str:
         logging.info("sending: " + message)
-        await telegram_client.send_message(BOT_USERNAME, message)
-    elif type(message) == dict:
+        await telegram_client.send_message(BOT_USERNAME, message) # type: ignore
+    elif type(message) is dict:
         if message.get("type") == "file":
-            file_path = message.get("path")
+            file_path: str = message["path"]
             logging.info("sending file: " + file_path)
-            await telegram_client.send_file(BOT_USERNAME, file_path)
+            await telegram_client.send_file(BOT_USERNAME, file_path) # type: ignore
         elif message.get("type") == "click-button":
-            buttons = await event.get_buttons()
-            if not buttons:
-                logging.error("can't find buttons to click: " +
-                              message.get("name"))
-                exit(1)
-            for bline in buttons:
-                for button in bline:
-                    if message.get("name") == button.button.text:
-                        logging.info("clicking: " + button.button.text)
-                        await button.click()
+            button_name = message["name"]
+            await click_inline_button(button_name, event)
 
 
 with telegram_client:
     @telegram_client.on(events.NewMessage(from_users=BOT_USERNAME))
     async def on_message_recieved(event):
-        await send_message(event)
+        logging.info("recieved message: " + event.message.message.split("\n")[0])
+        if error_occured or len(process) == 0:
+            disconn_coro = telegram_client.disconnect()
+            if disconn_coro: await disconn_coro
+        else:
+            await send_message(event)
+
     telegram_client.loop.run_until_complete(send_message())
     telegram_client.run_until_disconnected()
