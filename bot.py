1d532ca6033734ba2a0d3499906771189a1fcd70
محمد الصوالحي <ms.2052001@gmail.com>
2022-03-09 22:00:18 +0200

    script for bot in fovour of quick debugging

---
+++ bot.py
@@ -0,0 +1,61 @@
+import telebot
+from telebot import types
+from menu import main_menu
+from dotenv import dotenv_values
+
+env = dotenv_values(".env")
+bot = telebot.TeleBot(env.get("BOT_TOKEN"))
+
+current_path = []
+all_buttons = {}
+main_menu_copy = main_menu.copy()
+while len(main_menu_copy):
+    row = main_menu_copy.pop()
+    for button in row:
+        all_buttons[button["name"]] = button
+        if submenu := button.get("submenu"):
+            main_menu_copy.extend(submenu)
+
+
+def get_menu_markup(menu):
+    markup = types.ReplyKeyboardMarkup()
+    for row in menu:
+        markup.row(*[types.KeyboardButton(button["name"]) for button in row])
+    markup.row(types.KeyboardButton("◀️"), types.KeyboardButton("🔼"))
+    return markup
+
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    markup = get_menu_markup(main_menu)
+    bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+
+@bot.message_handler()
+def any_message(message):
+    button_name = message.text
+    if button_name == "◀️" and len(current_path) > 1:
+        button_name = current_path.pop()
+        button_name = current_path.pop()
+    if button_name == "🔼" or (button_name == "◀️" and len(current_path) == 1):
+        button = None
+        submenu = main_menu
+    else:
+        button = all_buttons.get(button_name)
+        submenu = button.get("submenu") if button else None
+        current_path.append(button_name)
+    if submenu:
+        markup = get_menu_markup(submenu)
+    else:
+        markup = None
+
+    bot.send_message(message.chat.id, message.text, reply_markup=markup)
+    messages = button.get("messages") if button else None
+    if messages:
+        for msg in messages:
+            if type(msg) is str:
+                bot.send_message(message.chat.id, msg, disable_web_page_preview=True)
+
+
+bot.set_webhook()
+bot.polling()
